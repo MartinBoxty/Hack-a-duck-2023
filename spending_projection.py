2ecc2e713d6667d6599c8d16c33b651565dcbde0
mpshmakov <104257422+mpshmakov@users.noreply.github.com>
2023-10-28 16:29:11 +0100

    added spending_projection.py

---
+++ spending_projection.py
@@ -0,0 +1,6 @@
+def spendingProj(transactions):
+    formula = 0
+    print(formula)
+    # we need to look at the dates of the transactions. after that we look at all of the transactions
+    # at that month and the other month (if there is one) and "return mean_average".
+    # if there is not enough data to calculate a mean for the user, we return 0.
