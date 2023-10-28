ef8d28eb2fbff56d253d27b0e384c89da34a55ac
mpshmakov <104257422+mpshmakov@users.noreply.github.com>
2023-10-28 20:04:30 +0100

    spending unfinisheddd

--- spending_projection.py
+++ spending_projection.py
@@ -1,18 +1,37 @@
 def spendingProj(transactions):
-    mean = 0
-    print("projected spendings = "+mean)
+    #mean = 0     not finished this yet
+    #print("projected spendings = "+str(mean))
     # we need to look at the dates of the transactions. after that we look at all of the transactions
     # at that month and the other month (if there is one) and "return mean_average".
     # if there is not enough data to calculate a mean for the user, we return 0.
 
+    Categories = {}  #  dic of categories by spendings and print from the highest to lowest
+    amounts = [{}]
 
-    highestCategory = {'mcdonalds':100000} # sorted dic of categories by spendings and print from the highest to lowest
-    print(highestCategory)
+    for transaction in transactions:
 
-    difference = 0; # how much you have spent so far compared to the prev month
-    print ("difference = "+difference)
+        if transaction["category"] not in Categories:
+            Categories[transaction["category"]] = transaction["amount"]
+        else:
+            Categories[transaction["category"]] += transaction["amount"]
+
+
+        amounts.append({"date": transaction["timestamp"], "money":transaction["amount"]})
+
+
+        if transaction["amount"] > highest_amount:
+            highest_amount = transaction["amount"]
+            highest_amount_transaction = transaction
+
+
+
+    for key, value in Categories.items():
+        print(f"{key}: {value}")
+
+    difference = 0;         # how much you have spent so far compared to the prev month
+    print ("difference = "+str(difference))
 
     # print the graph (in text format for now)
-    transactionsThisYear = {"29.10.23":0} # dic of spendings this year with date
+    transactionsThisYear = {"29.10.23":0} # dic of spendings this year with date. for a future bar chart
     for key, value in transactionsThisYear.items():
         print(f"{key}: {value}")
